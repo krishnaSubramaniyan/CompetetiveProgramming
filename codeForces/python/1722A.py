b319ea5675b43af894c71d47fd50577313e80d1b
krishna subramaniyan <krishnasubramaniyan2001@gmail.com>
2025-10-14 23:16:30 +0530

    A.Spell Check (if string is "Timur" in any order)

---
+++ codeForces/python/1722A.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    if (int(input()) != 5):
+        input()
+        print("NO")
+    else:
+        n = input()
+        if ('T' in n) and ('i' in n) and ('m' in n) and ('u' in n) and ('r' in n):
+            print("YES")
+        else:
+            print("NO")
